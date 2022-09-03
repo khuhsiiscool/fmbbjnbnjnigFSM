a03ea322b8c9893cd69e07ae90a75755af54f824
khuhsiiscool <93536796+khuhsiiscool@users.noreply.github.com>
2022-09-03 13:33:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
+status = "";
 function setup(){
     canvas=createCanvas(280,280);
     canvas.center();
-    background("white");
-video=createCapture(VIDEO);
-video.position(280,280);
-
 }
 function draw(){
     image(video, 0, 0, 280, 280);
+}
+video = "";
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide()
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects ";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
